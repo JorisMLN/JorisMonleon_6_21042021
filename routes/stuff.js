4b0c4acd9f492847bf4899494011ac874e196192
JorisMLN <monleon.joris@gmail.com>
2021-04-29 10:40:27 +0200

    multer + images

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const stuffCtrl =  require('../controllers/stuff');
+
 const auth = require('../middleware/auth');
+const stuffCtrl =  require('../controllers/stuff');
 
 router.post('/', auth,  stuffCtrl.createThing);
 router.put('/:id', auth, stuffCtrl.modifyThing);
