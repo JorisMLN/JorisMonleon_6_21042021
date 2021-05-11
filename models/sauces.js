bbaaaf484723fe58fd75975562f6aba8cad9c339
JorisMLN <monleon.joris@gmail.com>
2021-05-11 17:14:40 +0200

    Model sauce heat(min, max) required

--- models/sauces.js
+++ models/sauces.js
@@ -9,7 +9,7 @@ const sauceSchema = mongoose.Schema({
     description: {type: String, required: true},
     mainPepper: {type: String, required: true},
     imageUrl: {type: String, required: true},
-    heat: {type: Number, required: true},
+    heat: {type: Number, min: [0, 'Dat Child !!'], max: [10, 'Too much Heat !!'], required: true},
     likes: {type: Number, default: 0},
     dislikes: {type: Number, default: 0},
     usersLiked: {type: [String], default: []},
