f5fe8a0d121e5ede95e54bdbdf5b43d7c7c17d81
JorisMLN <monleon.joris@gmail.com>
2021-05-14 16:41:01 +0200

    MàJ MaskData 1/2

--- controllers/user.js
+++ controllers/user.js
@@ -5,14 +5,29 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+
+const maskData = require('maskdata');
+
+const emailMaskOptions = {
+    maskWith: "*", 
+    unmaskedStartCharactersBeforeAt: 0,
+    unmaskedEndCharactersAfterAt: 257,
+    maskAtTheRate: false
+};
+
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
+
+            let emailBeforeMask = req.body.email;
+            var maskedEmail = maskData.maskEmail2(emailBeforeMask, emailMaskOptions);
+
             const user = new User({
-                email: req.body.email,
+                email: maskedEmail,
                 password: hash
             });
             console.log(user);
+            
             user.save()
                 .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
                 .catch(error => res.status(400).json({ error }));
@@ -20,8 +35,25 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
+
+// exports.signup = (req, res, next) => {
+//     bcrypt.hash(req.body.password, 10)
+//         .then(hash => {
+//             const user = new User({
+//                 email: req.body.email,
+//                 password: hash
+//             });
+//             console.log(user);
+//             user.save()
+//                 .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+//                 .catch(error => res.status(400).json({ error }));
+//         })
+//         .catch(error => res.status(500).json({ error }));
+// };
+
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    // User.findOne({ email: req.body.email })
+    User.findOne({ maskedEmail })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
